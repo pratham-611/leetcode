93b2f97d88f1e17d59dcacc2cce3ca4b134429c5
pratham-611 <prathamkumargupta611@gmail.com>
2026-07-23 20:03:32 +0530

    Time: 4 ms (8.29%) | Memory: 16.6 MB (21.74%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,6 +11,27 @@
  */
 class Solution {
 public:
+
+    vector<int> ng;
+
+    void getNextGreater(vector<int>& preorder){
+        stack<int> st;
+        st.push(-1);
+        for(int i = preorder.size()-1; i>=0; i--){
+            if(st.top() == -1){
+                ng.push_back(-1);
+            }
+            else{
+                while(st.top() != -1 && preorder[i] >= preorder[st.top()]){
+                    st.pop();
+                }
+                ng.push_back(st.top());
+            }
+            st.push(i);
+        }
+        reverse(ng.begin(), ng.end());
+    }
+
     TreeNode* constructBst(vector<int>& preorder, int l, int r){
         if(l > r){
             return NULL;
@@ -20,13 +41,7 @@ public:
         if(l == r){
             return root;
         }
-        int maxi = -1;
-        for(int i = l+1; i<=r; i++){
-            if(preorder[l] < preorder[i]){
-                maxi = i;
-                break;
-            }
-        }
+        int maxi = ng[l];
         
         if(maxi != -1){
             TreeNode* left = constructBst(preorder,l+1, maxi-1);
@@ -44,6 +59,7 @@ public:
     }
 
     TreeNode* bstFromPreorder(vector<int>& preorder) {
+        getNextGreater(preorder);
         return constructBst(preorder, 0, preorder.size()-1);
     }
 };
